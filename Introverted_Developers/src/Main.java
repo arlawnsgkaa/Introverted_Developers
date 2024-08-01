b4d94b03e50a28166d7119a853d663ff3ada2e24
rlvuddl1234 <rlvuddl1234@naver.com>
2024-08-01 11:51:31 +0900

    변수추가

--- Introverted_Developers/src/Main.java
+++ Introverted_Developers/src/Main.java
@@ -2,6 +2,7 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        
+        int i = 90;
+        int k = 19;
     }
 }
